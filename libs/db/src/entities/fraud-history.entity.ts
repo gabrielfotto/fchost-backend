50909a3de637194f339edcfe6dfa1d549a2711b4
Gabriel <gabrielf.otto@hotmail.com>
2025-04-19 13:46:55 -0300

    wip: added FraudHistoryEntity

--- libs/db/src/entities/fraud-history.entity.ts
+++ libs/db/src/entities/fraud-history.entity.ts
@@ -4,17 +4,33 @@ import {
 	Entity,
 	JoinColumn,
 	ManyToOne,
+	OneToMany,
 	PrimaryGeneratedColumn,
 	UpdateDateColumn,
 } from 'typeorm'
 
-// import { AccountEntity } from '.'
 import { InvoiceEntity } from '.'
+import { EFraudReason } from '../../../shared/src/enums'
 
 @Entity({ name: 'fraud-history' })
 export default class FraudHistoryEntity {
 	@PrimaryGeneratedColumn()
 	id: number
+
+	@ManyToOne(() => InvoiceEntity, { onDelete: 'CASCADE' })
+	@JoinColumn({ name: 'invoiceId' })
+	invoice: InvoiceEntity
+
+	@Column({ type: 'enum', enum: EFraudReason })
+	reason: EFraudReason
+
+	@Column()
+	description: string
+
+	@OneToMany(() => FraudHistoryEntity, fraud => fraud.invoice, {
+		cascade: true,
+	})
+	fraudHistory: FraudHistoryEntity[]
 
 	@CreateDateColumn()
 	createdAt: Date
--- libs/db/src/entities/index.ts
+++ libs/db/src/entities/index.ts
@@ -1,7 +1,8 @@
-import { default as AccountEntity } from './account.entity'
-import { default as InvoiceEntity } from './invoice.entity'
+import AccountEntity from './account.entity'
+import InvoiceEntity from './invoice.entity'
+import FraudHistoryEntity from './fraud-history.entity'
 
-const Entities = [AccountEntity, InvoiceEntity]
+const Entities = [AccountEntity, InvoiceEntity, FraudHistoryEntity]
 
 export { AccountEntity, InvoiceEntity }
 
